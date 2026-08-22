172e20b8004451dbb6bdb99a619f366482de8de7
itsmebroarif <aripstrike@gmail.com>
2026-08-22 23:17:35 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Ini digunakan untuk menambah interaksi akan konfirmasi dan juga pengiriman data menuju ke whatsapp
+
+
+// Variabel ke whatsapp api
+const humanResourceContact = ""; // Ganti dengan nomor WhatsApp tujuan ke HR
+const staffProgrammerContact = ""; // Ganti dengan nomor WhatsApp tujuan ke Spv Programmer
